5a98a4a334a1f3ed0fe53e2a9e14d0dcc28b0be7
discite <discite@users.noreply.github.com>
2021-01-10 14:48:14 +0000

    deploy: discite/app_jebl@a9b3f76ed5255e4c6fb9970016cc230824bd2b33

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDhZ-jHE1c5XE-_zQiSLwnt0Zd34W24w08",
+    authDomain: "test-terreno-monitoreo.firebaseapp.com",
+    databaseURL: "https://test-terreno-monitoreo.firebaseio.com",
+    projectId: "test-terreno-monitoreo",
+    storageBucket: "test-terreno-monitoreo.appspot.com",
+    messagingSenderId: "790670405212",
+    appId: "1:790670405212:web:330314d06b59f64eaa8099"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
